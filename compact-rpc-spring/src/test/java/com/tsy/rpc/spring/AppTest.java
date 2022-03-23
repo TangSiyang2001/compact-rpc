d0e7277f3635223a1b8204637a3226b8bb7f4c4d
tsy <984337688@qq.com>
2022-03-23 23:00:14 +0800

    finish preliminary development stage

--- compact-rpc-spring/src/test/java/com/tsy/rpc/spring/AppTest.java
+++ compact-rpc-spring/src/test/java/com/tsy/rpc/spring/AppTest.java
@@ -1,20 +1,26 @@
 package com.tsy.rpc.spring;
 
-import static org.junit.Assert.assertTrue;
-
+import com.tsy.rpc.base.extension.ExtensionLoader;
+import com.tsy.rpc.remote.client.RequestSender;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testExtensionLoader() {
+        Assert.assertNotNull(ExtensionLoader.getExtensionLoader(RequestSender.class).getExtension("netty"));
     }
 }
